96e4be4753f67e58b069a4a3b1190a8953d7ee0f
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-11-01 13:50:37 +0100

    Added CORS middleware
    Closes #20

--- app/api.py
+++ app/api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, responses
+from fastapi.middleware.cors import CORSMiddleware
 from routers import ServerRouter, DockersRouter, DockerRouter, AuthRouter
 from pylib import pyUtils
 
 pyUtils.preBootCheck()
 
 api: FastAPI = FastAPI(title="RSMAPY", description="A python API to remotely manage a server", version="0.1", license_info={"name": "MIT License", "identifier": "MIT", "url": "https://github.com/zDany01/RSMAPY/blob/main/LICENSE"})
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(AuthRouter)
 api.include_router(ServerRouter)
